5512518fa4317ffc9e0f51c42fab5e0c9cbe1e0e
RufatGulabli <gulabli.rufat@gmail.com>
2019-01-17 17:40:04 +0400

    test on JEST

--- lib.js
+++ lib.js
@@ -14,8 +14,28 @@ function getCurrencies() {
     return ['USD', 'AZN', 'EUR'];
 }
 
+function getProduct(id) {
+    return { id: 1, price: 10, category: 'Tech' };
+}
+
+function registerUser(username) {
+    if (!username) throw new Error('Username is required.');
+    return { id: new Date().getTime(), username: username };
+}
+
+function fizzBuzz(input) {
+    if (typeof input !== 'number') throw new Error('Input should be a number');
+    if ((input % 3 === 0) && (input % 5) === 0) return 'FizzBuzz';
+    if(input % 3 === 0) return 'Fizz';
+    if(input % 5 === 0) return 'Buzz';
+    return input;
+}
+
 module.exports = {
     absolute: absolute,
     greet: greet,
-    getCurrencies: getCurrencies
+    getCurrencies: getCurrencies,
+    getProduct: getProduct,
+    registerUser: registerUser,
+    fizzBuzz: fizzBuzz
 }
--- tests/first.test.js
+++ tests/first.test.js
@@ -25,6 +25,7 @@ describe('greet', () => {
     });
 });
 
+/* Checking Array */
 describe('getCurrencies', () => {
     it('arrayTesting', () => {
         const result = lib.getCurrencies();
@@ -47,6 +48,66 @@ describe('getCurrencies', () => {
         // Ideal way
         expect(result).toEqual(expect.arrayContaining(['EUR', 'AZN', 'USD']));
     });
-})
+});
+
+/* Checking Object */
+describe('getProduct', () => {
+    it('object', () => {
+        const result = lib.getProduct(1);
+
+        // Incorrect, because it will check by reference, whereas these are 2 different objects
+        // expect(result).toBe({ id: 1, price: 10 });
+
+        // Matched objects must have exactly given properties
+        expect(result).toEqual({ id: 1, price: 10, category: 'Tech' });
+
+        // Matched objects should have at least given properties
+        expect(result).toMatchObject({ id: 1, price: 10 });
+
+        // Type of the id will be checked as well
+        expect(result).toHaveProperty('id', 1);
+    })
+});
+
+/*Chekcing Exceptions */
+describe('Exceptions', () => {
+    it('in case of Error throwing', () => {
+        const arr = [null, undefined, 0, false, '', NaN];
+        arr.forEach(a => { expect(() => { lib.registerUser(a) }).toThrow() });
+    });
+
+    it('in case of correct arguments', () => {
+        const result = lib.registerUser('Rufat');
+        expect(result).toMatchObject({ username: 'Rufat' });
+        expect(result.id).toBeGreaterThan(0);
+    });
+});
+
+// Exercise
+describe('FizzBuzz', () => {
+    it('should throw an exception if input is not a number', () => {
+        expect(() => { lib.fizzBuzz('a') }).toThrow();
+        expect(() => { lib.fizzBuzz(null) }).toThrow();
+        expect(() => { lib.fizzBuzz(undefined) }).toThrow();
+        expect(() => { lib.fizzBuzz({}) }).toThrow();
+    });
+
+    it(`should return 'FizzBuzz' if input is divizible by 5 & 3`, () => {
+        expect(lib.fizzBuzz(30)).toBe('FizzBuzz');
+    });
+
+    it(`should return 'Fizz' if input is divisible only by 3`, () => {
+        expect(lib.fizzBuzz(12)).toBe('Fizz');
+    });
+
+    it(`should return 'Buzz' if input is divisible only by 5`, () => {
+        expect(lib.fizzBuzz(20)).toBe('Buzz');
+    });
+
+    it(`should return input if input is not divisible by 3 or 5`, () => {
+        expect(lib.fizzBuzz(1)).toBe(1);
+    });
+
+});
 
 
